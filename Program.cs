bd1b1c3d40a1216ead8a2fb3fa6693acf65fe6b9
mswierkot <[markswierkot@gmail.com]>
2020-02-08 13:39:37 +0100

    Practicing c# delegates

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DelegatesPractice
+{
+    public delegate int MathematicalTransoformator(int x);
+    class Program
+    {
+      
+        static int Square (int x)
+        {
+            return x * x;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 1, 1, 2, 3, 5, 8, 13, 21 };
+            MathematicalTransoformator mathematicalTransoformator = Square;  
+
+            TraineeNo1 no1 = new TraineeNo1();
+            no1.CalculateSomething(ref array, mathematicalTransoformator);
+   
+            foreach(int a in array)
+            {
+                Console.WriteLine(a);
+            }    
+
+        } 
+
+
+    }
+}
---
+++ TraineeNo1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesPractice
+{
+    public class TraineeNo1
+    {
+
+        // whatever we plug-in to our method by delegate 'deleage', calculation will be different
+        // on that basis
+        public void CalculateSomething(ref int[] array, MathematicalTransoformator delegat)
+        {
+            for(int i =0; i < array.Length; i++)
+            {
+                array[i] = delegat(array[i]);
+            }
+
+        }
+         
+
+    }
+}
